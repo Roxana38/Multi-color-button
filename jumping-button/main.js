aba1b827d2dbf610b106d1ec055b58a0cb0ffb44
Roxana38 <98492894+Roxana38@users.noreply.github.com>
2022-09-20 09:54:29 +0300

    jumpin-button

--- jumping-button/main.js
+++ jumping-button/main.js
@@ -1,6 +1,13 @@
-function schimba(){
-    document.getElementById("flip-flop").style.right = "100px";
-    document.getElementById("flip-flop").innerHTML = "Go Left!";
-    
-}
-document.getElementsByTagName("button")[0].addEventListener('click', schimba);
+window.addEventListener("load", function(){ 
+    const button = document.getElementById('flip-flop');
+
+    button.addEventListener('click', event =>{
+        button.classList.toggle("right");   
+        if(button.textContent === 'Go Left!'){
+            button.textContent = `Go Right!`;
+        }else{
+            button.textContent = `Go Left!`;
+        }
+    });
+});
+  
